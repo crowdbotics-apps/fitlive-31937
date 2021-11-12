b5eeb01dc54df19be8ebd593042717aff62233cc
Crowdbotics <team@crowdbotics.com>
2021-11-12 19:42:29 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,12 +1,18 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
 
 export const globalOptions = {
   name: "fitlive_31937",
   url: "https://fitlive_31937.botics.co",
   api: "https://fitlive_31937.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
+    copy: "Routes available!"
+  },
+  undefined: {
+    title: "Class Length",
+    copy: "Routes available!",
+    styles: styles
   }
 }
